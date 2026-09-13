255025d288823b3f37d8198e9b5d44f51a9651e4
sojibhossein11h-ui <sojibhossein11h@gmail.com>
2026-09-13 11:12:04 +0600

    Refresh offline cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'home-healthcare-v1';
+const CACHE_NAME = 'home-healthcare-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
